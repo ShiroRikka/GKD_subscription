3e670460683ed61d4f8f1d84812e2a0285208eb2
ShiroRikka <shirorikka@qq.com>
2026-02-13 13:17:04 +0800

    ✨ feat(apps): 新增铁路12306应用规则 - 添加购票预填信息确定按钮点击规则 - 支持快速查询和快照匹配
    🔧 chore(subscription): 更新订阅配置
    - 修改订阅ID为5119
    - 设置作者为ShiroRikka
    - 添加版本号1
    
    🔧 chore(categories): 添加自定义分类
    - 添加"自用小工具"分类
    - 启用分类功能

--- src/categories.ts
+++ src/categories.ts
@@ -1,3 +1,9 @@
 import { defineGkdCategories } from '@gkd-kit/define';
 
-export default defineGkdCategories([]);
+export default defineGkdCategories([
+  {
+    key: 0,
+    name: '自用小工具',
+    enable: true,
+  },
+]);
--- src/subscription.ts
+++ src/subscription.ts
@@ -4,10 +4,10 @@ import categories from './categories';
 import globalGroups from './globalGroups';
 
 export default defineGkdSubscription({
-  id: 233,
+  id: 5119,
   name: 'Subscription',
-  version: 0,
-  author: 'author',
+  version: 1,
+  author: 'ShiroRikka',
   checkUpdateUrl: './gkd.version.json5',
   supportUri: 'https://github.com/gkd-kit/subscription-template',
   categories,
