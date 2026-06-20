480f8346be5609f4c2542e222f247f0de101d201
ShiroRikka <shirorikka@qq.com>
2026-06-20 12:26:52 +0800

    ✨ feat(bilibili): 优化演出票抢购规则并移除冗余逻辑
    - 移除已失效的“进入抢票页面”规则
    - 精简“购票/预定”及“提交订单”规则，去除重复匹配项
    - 调整规则执行策略，设置强制超时时间并重置冷却间隔

--- src/apps/tv.danmaku.bili.ts
+++ src/apps/tv.danmaku.bili.ts
@@ -6,63 +6,35 @@ export default defineGkdApp({
   groups: [
     {
       key: 1,
-      name: '自动点击-演出票-进入抢票页面',
-      desc: '开票前点击"即将开售"进入半屏页面，每次进入该页面只触发一次',
-      fastQuery: true,
-      rules: [
-        {
-          key: 1,
-          name: '点击即将开售',
-          matches: 'Button[text="即将开售"]',
-          action: 'clickCenter',
-          actionMaximum: 1,
-          actionCd: 2000,
-        },
-      ],
-    },
-    {
-      key: 2,
       name: '自动点击-演出票-购票/预定按钮',
       desc: '倒计时结束后自动点击变为可点击状态的"立即购票"或"立即预定"按钮',
       fastQuery: true,
-      actionCd: 50,
+      forcedTime: 7200000,
+      actionCd: 0,
       rules: [
         {
           key: 1,
-          name: '点击立即购票1',
+          name: '点击立即购票',
           matches: 'TextView[text*="合计"] +(n) Button[text="立即购票"]',
           excludeMatches: '[text^="请选择"]',
           action: 'clickCenter',
         },
         {
           key: 2,
-          name: '点击立即预定1',
+          name: '点击立即预定',
           matches: 'TextView[text*="合计"] +(n) Button[text="立即预定"]',
-          excludeMatches: '[text^="请选择"]',
-          action: 'clickCenter',
-        },
-        {
-          key: 3,
-          name: '点击立即购票2',
-          matches: 'Button[text="立即购票"]',
-          excludeMatches: '[text^="请选择"]',
-          action: 'clickCenter',
-        },
-        {
-          key: 4,
-          name: '点击立即预定2',
-          matches: 'Button[text="立即预定"]',
           excludeMatches: '[text^="请选择"]',
           action: 'clickCenter',
         },
       ],
     },
     {
-      key: 3,
+      key: 2,
       name: '自动点击-演出票-提交订单循环',
       desc: '自动循环点击"再试一次"；"再试一次"消失后立即点击"提交订单"',
       fastQuery: true,
-      actionCd: 50,
+      forcedTime: 7200000,
+      actionCd: 0,
       rules: [
         {
           key: 1,
@@ -72,15 +44,8 @@ export default defineGkdApp({
         },
         {
           key: 2,
-          name: '点击提交订单1',
+          name: '点击提交订单',
           matches: 'TextView[text*="合计"] +(n) Button[text="提交订单"]',
-          excludeMatches: '[text="再试一次"]',
-          action: 'clickCenter',
-        },
-        {
-          key: 3,
-          name: '点击提交订单2',
-          matches: 'Button[text="提交订单"]',
           excludeMatches: '[text="再试一次"]',
           action: 'clickCenter',
         },
